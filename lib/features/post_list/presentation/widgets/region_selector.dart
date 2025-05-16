1e42a5a8c710cf4df360368b6d9d317439391269
hippieperm <leess0133@gmail.com>
2025-05-16 21:34:00 +0900

    feat: 지역 선택기 위젯 추가 - 게시물 목록에서 지역 선택 기능 구현

---
+++ lib/features/post_list/presentation/widgets/region_selector.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:sodong_app/features/post_list/presentation/view_models/town_life_view_model.dart';
+
+class RegionSelector extends ConsumerWidget {
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final categories = ref.watch(categoriesProvider);
+    final selectedCategory = ref.watch(selectedCategoryProvider);
+
+    return Container(
+      height: 50,
+    );
+  }
+}
