7daf8f849684b13d21a176c11d606fa1ac853f27
dlworua <leegaegyum@gmail.com>
2025-05-16 22:50:26 +0900

    feat: loginPage UI 기본 뼈대 구현

--- lib/features/auth/presentation/pages/login/login_page.dart
+++ lib/features/auth/presentation/pages/login/login_page.dart
@@ -6,9 +6,21 @@ class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('안녕하세옹'),
-      ),
+      body: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
+        Image.asset('assets/login.png'),
+        SizedBox(
+          height: 40,
+        ),
+        Row(mainAxisAlignment: MainAxisAlignment.center, children: [
+          // 구글 로고 (이미지 사용 권장)
+          Image.asset('assets/google.png', height: 30),
+          const SizedBox(width: 16),
+          Text(
+            '구글 계정으로 시작하기',
+            style: const TextStyle(fontSize: 20),
+          )
+        ])
+      ]),
     );
   }
 }
