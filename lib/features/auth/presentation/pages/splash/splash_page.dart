d7d2c66aaba823f95200b329cced0c8ee65bf1f7
dlworua <leegaegyum@gmail.com>
2025-05-16 22:03:48 +0900

    feat: 스플레시 페이지 메서드 위치이동

--- lib/features/auth/presentation/pages/splash/splash_page.dart
+++ lib/features/auth/presentation/pages/splash/splash_page.dart
@@ -9,19 +9,6 @@ class SplashPage extends StatefulWidget {
 
 class _SplashPageState extends State<SplashPage> {
   @override
-  void initState() {
-    super.initState();
-
-    _goToLogin();
-  }
-
-  void _goToLogin() async {
-    await Future.delayed(const Duration(seconds: 2));
-    if (!mounted) return;
-    await Navigator.pushReplacementNamed(context, '/login');
-  }
-
-  @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: const Color(0xFFFFE6E9),
@@ -33,5 +20,18 @@ class _SplashPageState extends State<SplashPage> {
         ),
       ),
     );
+  }
+
+  @override
+  void initState() {
+    super.initState();
+
+    _goToLogin();
+  }
+
+  void _goToLogin() async {
+    await Future.delayed(const Duration(seconds: 2));
+    if (!mounted) return;
+    await Navigator.pushReplacementNamed(context, '/login');
   }
 }
