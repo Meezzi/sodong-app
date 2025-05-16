ea7984dab18ca0f644454f3320cd47e759897e88
dlworua <leegaegyum@gmail.com>
2025-05-16 20:59:08 +0900

    feat: 실제 상태와 UI를 관리하는 클래스 만들고 기본 UI 구현

--- lib/features/auth/presentation/pages/splash/splash_page.dart
+++ lib/features/auth/presentation/pages/splash/splash_page.dart
@@ -4,8 +4,21 @@ class SplashPage extends StatefulWidget {
   const SplashPage({super.key});
 
   @override
-  State<StatefulWidget> createState() {
-    // TODO: implement createState
-    throw UnimplementedError();
+  State<StatefulWidget> createState() => _SplashPageState();
+}
+
+class _SplashPageState extends State<SplashPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: const Color(0xFFFFE6E9),
+      body: Center(
+        child: Image.asset(
+          'assets/splash.png',
+          width: 250,
+          height: 250,
+        ),
+      ),
+    );
   }
 }
