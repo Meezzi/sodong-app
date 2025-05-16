ceb54138be383b870ae812fccadda39eb2c3ff35
dlworua <leegaegyum@gmail.com>
2025-05-16 20:50:57 +0900

    feat: splashPage의 상태가 바뀔 수 있도록 StatefulWidget으로 클래스 생성

---
+++ lib/features/auth/presentation/pages/splash/splash_page.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class SplashPage extends StatefulWidget {
+  const SplashPage({super.key});
+
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    throw UnimplementedError();
+  }
+}
