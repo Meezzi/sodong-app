2b0df5b160303eac5a91baa30eccb3ab7d25b5a9
hippieperm <leess0133@gmail.com>
2025-05-16 18:52:47 +0900

    feat: TownLifePage 클래스 추가 - 지역 생활 페이지의 기본 구조 구현

---
+++ lib/features/post_list/presentation/pages/town_life_page.dart
@@ -0,0 +1,9 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+class TownLifePage extends ConsumerStatefulWidget {
+  @override
+  ConsumerState<ConsumerStatefulWidget> createState() {
+    // TODO: implement createState
+    throw UnimplementedError();
+  }
+}
