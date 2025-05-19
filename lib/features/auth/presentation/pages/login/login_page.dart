edcdcc910749c22516c15630706ef631c7c6c94d
dlworua <leegaegyum@gmail.com>
2025-05-19 14:41:55 +0900

    feat: Log in Page UI 구현  완료

--- lib/features/auth/presentation/pages/login/login_page.dart
+++ lib/features/auth/presentation/pages/login/login_page.dart
@@ -6,21 +6,34 @@ class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
-        Image.asset('assets/login.png'),
-        SizedBox(
-          height: 40,
+      body: Center(
+        // 중앙 정렬 보장
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Image.asset('assets/login.png'),
+            const SizedBox(height: 40),
+            Container(
+              padding: const EdgeInsets.symmetric(horizontal: 24, vertical: 12),
+              decoration: BoxDecoration(
+                border: Border.all(color: Colors.grey), // 테두리 추가
+                borderRadius: BorderRadius.circular(8), // 둥근 모서리
+              ),
+              child: Row(
+                mainAxisSize: MainAxisSize.min, // 내용 크기만큼만 차지
+                children: [
+                  Image.asset('assets/google.png', height: 30),
+                  const SizedBox(width: 16),
+                  const Text(
+                    '구글 계정으로 시작하기',
+                    style: TextStyle(fontSize: 18),
+                  ),
+                ],
+              ),
+            ),
+          ],
         ),
-        Row(mainAxisAlignment: MainAxisAlignment.center, children: [
-          // 구글 로고 (이미지 사용 권장)
-          Image.asset('assets/google.png', height: 30),
-          const SizedBox(width: 16),
-          Text(
-            '구글 계정으로 시작하기',
-            style: const TextStyle(fontSize: 20),
-          )
-        ])
-      ]),
+      ),
     );
   }
 }
