587ec97629c21622c799898da77207b8e8fa330b
jinsung99123 <felixson99@gmail.com>
2025-05-19 16:26:15 +0900

    feat: Post Detail Page 파이어베이스 get 코드 틀 구현

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:sodong_app/features/auth/presentation/pages/login/login_page.dart';
