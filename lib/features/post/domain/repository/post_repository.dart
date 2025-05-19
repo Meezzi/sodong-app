7f348b27c129fd38be41e07e0bcc7a933b18759d
Meezzi <alswl-3016@naver.com>
2025-05-19 18:11:35 +0900

    feat: PostRepository 인터페이스 추가

---
+++ lib/features/post/domain/repository/post_repository.dart
@@ -0,0 +1,12 @@
+import 'package:sodong_app/core/result/result.dart';
+import 'package:sodong_app/features/post/domain/entities/post.dart';
+import 'package:sodong_app/features/post_list/domain/models/category.dart';
+
+abstract interface class PostRepository {
+  /// Post 저장
+  Future<Result<void>> savePost(
+    String location,
+    TownLifeCategory category,
+    Post post,
+  );
+}
