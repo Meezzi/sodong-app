776a62394d91eded01d9137331a89da60a082b68
hippieperm <leess0133@gmail.com>
2025-05-19 20:28:30 +0900

    fix: 현재 선택된 지역을 부산에서 서울로 변경

--- lib/features/post_list/data/repository/post_repository.dart
+++ lib/features/post_list/data/repository/post_repository.dart
@@ -5,12 +5,11 @@ import 'package:sodong_app/features/post_list/domain/models/firestore_post.dart'
 import 'package:sodong_app/features/post_list/domain/models/region.dart';
 
 class PostRepository {
-  // 싱글톤으로 구성
-  static final PostRepository _instance = PostRepository._internal();
-
+  PostRepository._internal();
   // 생성자를 다른 멤버 선언 전에 배치
   factory PostRepository() => _instance;
-  PostRepository._internal();
+  // 싱글톤으로 구성
+  static final PostRepository _instance = PostRepository._internal();
 
   static const int pageSize = 10;
   int _currentPage = 0;
@@ -20,7 +19,7 @@ class PostRepository {
   int _totalItemCount = 0; // 전체 문서 수를 저장
 
   // 현재 선택된 지역과 카테고리
-  String _currentRegionId = 'busan';
+  String _currentRegionId = 'seoul';
   String _currentCategory = 'question';
 
   // Firestore 인스턴스
