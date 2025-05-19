3815e86e8ad7ab2d1154e7978bac5124b5a30b2f
jinsung99123 <felixson99@gmail.com>
2025-05-19 16:18:24 +0900

    feat: 파이어베이스 설정

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_core/firebase_core.dart' show FirebaseOptions;
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:sodong_app/features/auth/presentation/pages/login/login_page.dart';
