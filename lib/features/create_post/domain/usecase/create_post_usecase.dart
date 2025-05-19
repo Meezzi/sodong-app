4ccdd4155f497b52b432f8d7eed4ee644b52b684
Meezzi <alswl-3016@naver.com>
2025-05-19 21:06:48 +0900

    feat: CreatePostUsecase에 provider 추가

--- lib/features/create_post/domain/usecase/create_post_usecase.dart
+++ lib/features/create_post/domain/usecase/create_post_usecase.dart
@@ -1,11 +1,13 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:sodong_app/core/result/result.dart';
+import 'package:sodong_app/features/post/data/remote_post_repository.dart';
 import 'package:sodong_app/features/post/domain/entities/post.dart';
 import 'package:sodong_app/features/post/domain/repository/post_repository.dart';
 import 'package:sodong_app/features/post_list/domain/models/category.dart';
 
 class CreatePostUsecase {
   CreatePostUsecase(this._repository);
-  
+
   final PostRepository _repository;
 
   Future<Result<Post>> execute({
@@ -16,3 +18,10 @@ class CreatePostUsecase {
     return _repository.savePost(location, category, post);
   }
 }
+
+final createPostUsecaseProvider = Provider(
+  (ref) {
+    final _repository = ref.read(postRepositoryProvider);
+    return CreatePostUsecase(_repository);
+  },
+);
