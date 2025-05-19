e6709f9d877f7fafd6e5db202f6e850a5384287c
Meezzi <alswl-3016@naver.com>
2025-05-19 21:03:40 +0900

    feat: CreatePostUsecase에 게시물 저장 기능 구현

---
+++ lib/features/create_post/domain/usecase/create_post_usecase.dart
@@ -0,0 +1,18 @@
+import 'package:sodong_app/core/result/result.dart';
+import 'package:sodong_app/features/post/domain/entities/post.dart';
+import 'package:sodong_app/features/post/domain/repository/post_repository.dart';
+import 'package:sodong_app/features/post_list/domain/models/category.dart';
+
+class CreatePostUsecase {
+  CreatePostUsecase(this._repository);
+  
+  final PostRepository _repository;
+
+  Future<Result<Post>> execute({
+    required String location,
+    required TownLifeCategory category,
+    required Post post,
+  }) {
+    return _repository.savePost(location, category, post);
+  }
+}
