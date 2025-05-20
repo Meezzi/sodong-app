d403b576c506b024f4f65d8801e8dcc5d7aa3a58
Meezzi <alswl-3016@naver.com>
2025-05-20 22:32:43 +0900

    feat: ImagePickerViewModel 및 상태 관리 추가

---
+++ lib/features/create_post/presentation/view_models/image_picker_view_model.dart
@@ -0,0 +1,62 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:sodong_app/features/create_post/domain/usecase/pick_image_usecase.dart';
+import 'package:sodong_app/features/create_post/presentation/view_models/create_post_view_model.dart';
+
+class ImagePickerState {
+  ImagePickerState({
+    this.imageFiles,
+    this.isLoading = false,
+    this.error,
+  });
+
+  final List<XFile>? imageFiles;
+  final bool isLoading;
+  final String? error;
+
+  ImagePickerState copyWith({
+    List<XFile>? imageFiles,
+    bool? isLoading,
+    String? error,
+  }) {
+    return ImagePickerState(
+      imageFiles: imageFiles ?? this.imageFiles,
+      isLoading: isLoading ?? this.isLoading,
+      error: error ?? this.error,
+    );
+  }
+}
+
+class ImagePickerViewModel extends Notifier<ImagePickerState> {
+  @override
+  ImagePickerState build() {
+    return ImagePickerState();
+  }
+
+  Future<void> pickImages() async {
+    try {
+      state = state.copyWith(isLoading: true);
+      final imagePickerUsecase = ref.read(pickImagesUsecaseProvider);
+      final pickedFiles = await imagePickerUsecase.execute();
+      state = state.copyWith(
+        imageFiles: pickedFiles,
+        isLoading: false,
+      );
+
+      final createPostNotifier = ref.read(createPostViewModelProvider.notifier);
+      for (var file in pickedFiles) {
+        createPostNotifier.addImage(file.path);
+      }
+    } catch (e) {
+      state = state.copyWith(
+        isLoading: false,
+        error: '$e',
+      );
+    }
+  }
+}
+
+final imagePickerViewModelProvider =
+    NotifierProvider<ImagePickerViewModel, ImagePickerState>(() {
+  return ImagePickerViewModel();
+});
