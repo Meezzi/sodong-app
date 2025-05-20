c3aa7d8847c3e397ecbe31da99494863ac722959
jinsung99123 <felixson99@gmail.com>
2025-05-20 14:33:30 +0900

    feat: Post Detail Page tuple적용 및 쿼리 설정

--- lib/post_detail/data/providers/chat_detail_providers.dart
+++ lib/post_detail/data/providers/chat_detail_providers.dart
@@ -1,20 +1,26 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:sodong_app/post_detail/data/dtos/post_detail_dto.dart';
 import 'package:sodong_app/post_detail/data/repository/post_detail_repository.dart';
+import 'package:tuple/tuple.dart';
 
-final postIdProvider = StateProvider<String?>((ref) => null);
+final postInfoProvider = StateProvider<Tuple3<String?, String?, String?>>(
+    (ref) => const Tuple3(null, null, null));
 
 final postRepositoryProvider = Provider<PostDetailRepository>((ref) {
   return PostDetailRepository();
 });
 
 final postStreamProvider = StreamProvider<PostDetail>((ref) {
-  final postId = ref.watch(postIdProvider);
+  final postInfo = ref.watch(postInfoProvider);
   final repository = ref.read(postRepositoryProvider);
 
-  if (postId == null) {
-    throw Exception('postId가 설정되지 않았습니다.');
+  final location = postInfo.item1;
+  final category = postInfo.item2;
+  final postId = postInfo.item3;
+
+  if (location == null || category == null || postId == null) {
+    throw Exception('location/category/postId가 설정되지 않았습니다.');
   }
 
-  return repository.getPostStream(postId);
+  return repository.getPostStream(location, category, postId);
 });
--- lib/post_detail/data/repository/post_detail_repository.dart
+++ lib/post_detail/data/repository/post_detail_repository.dart
@@ -2,9 +2,13 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:sodong_app/post_detail/data/dtos/post_detail_dto.dart';
 
 class PostDetailRepository {
-  Stream<PostDetail> getPostStream(String postId) {
+  Stream<PostDetail> getPostStream(
+      String location, String category, String postId) {
     return FirebaseFirestore.instance
-        .doc('posts/busan/news/$postId')
+        .collection('posts')
+        .doc(location)
+        .collection(category)
+        .doc(postId)
         .snapshots()
         .map((doc) => PostDetail.fromJson(doc.data() ?? {}));
   }
