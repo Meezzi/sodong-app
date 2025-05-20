685fbaa7037819307f562929aef578da01553cb0
jinsung99123 <felixson99@gmail.com>
2025-05-20 11:18:04 +0900

    feat: Post Detail 기본 파이어베이스 레포지토리 생성

---
+++ lib/post_detail/data/repository/post_detail_repository.dart
@@ -0,0 +1,11 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:sodong_app/post_detail/data/dtos/post_detail_dto.dart';
+
+class PostRepository {
+  Stream<PostDetail> getPostStream(String postId) {
+    return FirebaseFirestore.instance
+        .doc('posts/busan/news/$postId')
+        .snapshots()
+        .map((doc) => PostDetail.fromJson(doc.data()!));
+  }
+}
