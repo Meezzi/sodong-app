2117780fec292d58e309c01963f186897c64e61e
dlworua <leegaegyum@gmail.com>
2025-05-20 22:54:24 +0900

    feat: 구글로그인 할때 필요한 코드 수정

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -1,3 +1,15 @@
+buildscript {
+    repositories {
+        google()
+        mavenCentral()
+    }
+    dependencies {
+        classpath 'com.android.tools.build:gradle:8.1.1' // 예시, 버전 확인 필요
+        classpath 'com.google.gms:google-services:4.3.15' // Google Services plugin
+        // 기타 classpath 의존성들
+    }
+}
+
 allprojects {
     repositories {
         google()
@@ -5,6 +17,7 @@ allprojects {
     }
 }
 
+// buildDir 이동 커스텀 설정
 val newBuildDir: Directory = rootProject.layout.buildDirectory.dir("../../build").get()
 rootProject.layout.buildDirectory.value(newBuildDir)
 
@@ -12,6 +25,7 @@ subprojects {
     val newSubprojectBuildDir: Directory = newBuildDir.dir(project.name)
     project.layout.buildDirectory.value(newSubprojectBuildDir)
 }
+
 subprojects {
     project.evaluationDependsOn(":app")
 }
