72e67257c8d2d1567018a65db4df0eada5324f4b
hippieperm <leess0133@gmail.com>
2025-05-20 22:02:30 +0900

    feat: PostRepository 인터페이스 추가 및 게시물 데이터 관리 메서드 정의

---
+++ lib/features/post_list/domain/repositories/post_repository.dart
@@ -0,0 +1,26 @@
+import 'package:sodong_app/features/post_list/domain/models/region.dart';
+import 'package:sodong_app/features/post_list/domain/models/town_life_post.dart';
+
+/// 게시물 저장소 인터페이스
+abstract class PostRepository {
+  /// 초기 게시물 가져오기
+  Future<List<TownLifePost>> fetchInitialPosts();
+
+  /// 추가 게시물 가져오기 (페이지네이션)
+  Future<List<TownLifePost>> fetchMorePosts();
+
+  /// 현재 지역에 대한 특정 카테고리 게시물 가져오기
+  Future<List<TownLifePost>> fetchCurrentRegionCategoryPosts(String categoryId);
+
+  /// 지역 설정
+  void setRegion(Region region);
+
+  /// 하위 지역 설정
+  void setSubRegion(String subRegion);
+
+  /// 카테고리 설정
+  void setCategory(String category);
+
+  /// 추가 게시물 존재 여부
+  bool get hasMorePosts;
+}
