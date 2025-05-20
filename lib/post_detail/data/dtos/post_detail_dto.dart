0a650561a0ee15115dd6f0f714c7d6e80f1941b5
jinsung99123 <felixson99@gmail.com>
2025-05-20 11:18:04 +0900

    feat: Post Detail 기본 파이어베이스 레포지토리 생성

--- lib/post_detail/data/dtos/post_detail_dto.dart
+++ lib/post_detail/data/dtos/post_detail_dto.dart
@@ -1,25 +1,22 @@
 class PostDetail {
-  final String id;
   final String title;
   final String content;
-  final String imageUrl;
   final String location;
+  final String imageUrl;
 
   PostDetail({
-    required this.id,
     required this.title,
     required this.content,
-    required this.imageUrl,
     required this.location,
+    required this.imageUrl,
   });
 
-  factory PostDetail.fromDocument(String id, Map<String, dynamic> json) {
+  factory PostDetail.fromJson(Map<String, dynamic> json) {
     return PostDetail(
-      id: id,
       title: json['title'] ?? '제목 없음',
       content: json['content'] ?? '내용 없음',
-      imageUrl: json['imageUrl'] ?? '',
       location: json['location'] ?? '위치 없음',
+      imageUrl: json['imageUrl'] ?? '',
     );
   }
 }
