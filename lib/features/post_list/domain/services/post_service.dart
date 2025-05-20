dca2d223b2aedb7388a029a73802119cafc84f79
hippieperm <leess0133@gmail.com>
2025-05-20 22:02:37 +0900

    feat: 게시물 관련 비즈니스 로직을 처리하는 PostService 클래스 추가

---
+++ lib/features/post_list/domain/services/post_service.dart
@@ -0,0 +1,44 @@
+import 'package:sodong_app/features/post_list/domain/models/region.dart';
+import 'package:sodong_app/features/post_list/domain/models/town_life_post.dart';
+import 'package:sodong_app/features/post_list/domain/repositories/post_repository.dart';
+
+/// 게시물 관련 비즈니스 로직을 담당하는 서비스
+class PostService {
+  final PostRepository _repository;
+
+  PostService(this._repository);
+
+  /// 초기 게시물 조회 UseCase
+  Future<List<TownLifePost>> getInitialPosts() async {
+    return await _repository.fetchInitialPosts();
+  }
+
+  /// 추가 게시물 조회 UseCase
+  Future<List<TownLifePost>> getMorePosts() async {
+    return await _repository.fetchMorePosts();
+  }
+
+  /// 현재 지역 특정 카테고리 게시물 조회 UseCase
+  Future<List<TownLifePost>> getCurrentRegionCategoryPosts(
+      String categoryId) async {
+    return await _repository.fetchCurrentRegionCategoryPosts(categoryId);
+  }
+
+  /// 지역 설정 UseCase
+  void setRegion(Region region) {
+    _repository.setRegion(region);
+  }
+
+  /// 하위 지역 설정 UseCase
+  void setSubRegion(String subRegion) {
+    _repository.setSubRegion(subRegion);
+  }
+
+  /// 카테고리 설정 UseCase
+  void setCategory(String category) {
+    _repository.setCategory(category);
+  }
+
+  /// 추가 게시물 존재 여부 확인
+  bool get hasMorePosts => _repository.hasMorePosts;
+}
