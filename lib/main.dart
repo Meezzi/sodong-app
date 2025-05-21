e5d6d9107d8076c4c0770cc88d46704c4970214c
dlworua <leegaegyum@gmail.com>
2025-05-21 20:12:35 +0900

    feat: 홈으로 가는 경로 코드 삭제

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,8 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:sodong_app/features/auth/presentation/pages/%08profile_edit/profile_edit.dart';
 import 'package:sodong_app/features/auth/presentation/pages/login/login_page.dart';
-import 'package:sodong_app/features/auth/presentation/pages/signup/signup_page.dart';
 import 'package:sodong_app/features/auth/presentation/pages/splash/splash_page.dart';
 import 'package:sodong_app/firebase_options.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
@@ -27,8 +27,7 @@ class MyApp extends StatelessWidget {
       routes: {
         '/': (context) => const SplashPage(),
         '/login': (context) => const LoginPage(),
-        '/signup': (context) => const SignUpPage(),
-        '/home': (context) => const SignUpPage(),
+        '/signup': (context) => const ProfileEdit(),
       },
     );
   }
