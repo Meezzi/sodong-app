bc380e8d9a0876ad8fb7422acd550f5b83e88c00
jinsung99123 <felixson99@gmail.com>
2025-05-22 15:31:00 +0900

    feat: 프로필 기능 entity 생성

---
+++ lib/features/profile/domain/entities/profile_entity.dart
@@ -0,0 +1,10 @@
+class ProfileEntity {
+  ProfileEntity({
+    required this.nickname,
+    required this.imageUrl,
+    required this.location,
+  });
+  final String nickname;
+  final String imageUrl;
+  final String location;
+}
