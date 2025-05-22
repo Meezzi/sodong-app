980ee6e1d5db1aa04a46c4f6e0c4897f4f9346bb
Meezzi <alswl-3016@naver.com>
2025-05-22 11:49:20 +0900

    fix: create_post route 생성

--- lib/main.dart
+++ lib/main.dart
@@ -5,8 +5,9 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:sodong_app/features/auth/presentation/pages/login/login_page.dart';
 import 'package:sodong_app/features/auth/presentation/pages/profile_edit/profile_edit.dart';
 import 'package:sodong_app/features/auth/presentation/pages/splash/splash_page.dart';
-import 'package:sodong_app/features/firebase_options.dart';
+import 'package:sodong_app/features/create_post/presentation/pages/create_post_page.dart';
 import 'package:sodong_app/features/post_list/presentation/pages/post_list_page.dart';
+import 'package:sodong_app/firebase_options.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -30,6 +31,7 @@ class MyApp extends StatelessWidget {
         '/login': (context) => const LoginPage(),
         '/signup': (context) => const ProfileEdit(),
         '/home': (context) => const PostListPage(),
+        '/create_post': (context) => const CreatePostPage(),
       },
     );
   }
