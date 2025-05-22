a8f8eaba35207e778542ae12a12fa8fbf78bc038
hippieperm <leess0133@gmail.com>
2025-05-22 23:01:02 +0900

    feat: 글쓰기 페이지의 텍스트 필드 UI 개선 및 스타일 추가

--- lib/features/create_post/presentation/pages/widgets/content_text_field.dart
+++ lib/features/create_post/presentation/pages/widgets/content_text_field.dart
@@ -4,19 +4,40 @@ class _ContentTextField extends StatelessWidget {
   const _ContentTextField({
     required this.notifier,
   });
-  
+
   final CreatePostViewModel notifier;
 
   @override
   Widget build(BuildContext context) {
-    return Expanded(
+    return Container(
+      height: 200,
+      decoration: BoxDecoration(
+        color: const Color(0xFFFAFAFA),
+        borderRadius: BorderRadius.circular(20),
+        border: Border.all(
+          color: const Color(0xFFFFE4E8),
+          width: 1.0,
+        ),
+      ),
+      padding: const EdgeInsets.all(16),
       child: TextField(
         decoration: const InputDecoration(
           hintText: '이야기를 들려주세요.',
+          hintStyle: TextStyle(
+            color: Colors.grey,
+            fontSize: 16,
+          ),
           border: InputBorder.none,
+          contentPadding: EdgeInsets.zero,
+        ),
+        style: const TextStyle(
+          fontSize: 16,
+          color: Colors.black87,
+          height: 1.5,
         ),
         maxLines: null,
         expands: true,
+        textAlignVertical: TextAlignVertical.top,
         onChanged: notifier.setContent,
       ),
     );
