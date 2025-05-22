8fdbcd502ee7cc32afb9de0853990ed2b4032c8c
Meezzi <alswl-3016@naver.com>
2025-05-22 23:29:16 +0900

    feat: ConsumerStatefulWidget을 적용하고 사용자 상태 확인 로직 구현

--- lib/features/auth/presentation/pages/splash/splash_page.dart
+++ lib/features/auth/presentation/pages/splash/splash_page.dart
@@ -1,20 +1,22 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:sodong_app/features/auth/domain/usecase/check_user_status_usecase.dart';
 
-class SplashPage extends StatefulWidget {
+class SplashPage extends ConsumerStatefulWidget {
   const SplashPage({super.key});
 
   @override
-  State<StatefulWidget> createState() => _SplashPageState();
+  ConsumerState<SplashPage> createState() => _SplashPageState();
 }
 
-class _SplashPageState extends State<SplashPage> {
+class _SplashPageState extends ConsumerState<SplashPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: const Color(0xFFFFE6E9),
       body: Center(
-        child: Image.asset(
-          'assets/splash.png',
+        child: Image(
+          image: AssetImage('assets/splash.png'),
           width: 250,
           height: 250,
         ),
@@ -26,12 +28,30 @@ class _SplashPageState extends State<SplashPage> {
   void initState() {
     super.initState();
 
-    _goToLogin();
+    WidgetsBinding.instance.addPostFrameCallback((_) => _checkUserStatus());
   }
 
-  void _goToLogin() async {
+  Future<void> _checkUserStatus() async {
     await Future.delayed(const Duration(seconds: 2));
+
+    final useCase = ref.read(checkUserStatusUsecaseProvider);
+    final status = await useCase.execute();
+
+    switch (status) {
+      case UserStatus.notLoggedIn:
+        _navigateTo('/login');
+        break;
+      case UserStatus.agreementNotComplete:
+        _navigateTo('/agreement');
+        break;
+      case UserStatus.agreementComplete:
+        _navigateTo('/home');
+        break;
+    }
+  }
+
+  void _navigateTo(String route) {
     if (!mounted) return;
-    await Navigator.pushReplacementNamed(context, '/agreement');
+    Navigator.pushReplacementNamed(context, route);
   }
 }
