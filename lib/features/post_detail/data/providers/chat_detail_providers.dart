7d0481e3308f4ecd55161c6bbce1c389dd89ff84
Meezzi <alswl-3016@naver.com>
2025-05-22 12:00:51 +0900

    refactor: postStreamProvider를 StateProvider에서 StreamProvider.autoDispose로 변경하여 인자 처리 개선

--- lib/features/post_detail/data/providers/chat_detail_providers.dart
+++ lib/features/post_detail/data/providers/chat_detail_providers.dart
@@ -3,20 +3,17 @@ import 'package:sodong_app/features/post_detail/data/dtos/post_detail_dto.dart';
 import 'package:sodong_app/features/post_detail/data/repository/post_detail_repository.dart';
 import 'package:tuple/tuple.dart';
 
-final postInfoProvider = StateProvider<Tuple3<String?, String?, String?>>(
-    (ref) => const Tuple3(null, null, null));
-
 final postRepositoryProvider = Provider<PostDetailRepository>((ref) {
   return PostDetailRepository();
 });
 
-final postStreamProvider = StreamProvider<PostDetail>((ref) {
-  final postInfo = ref.watch(postInfoProvider);
+final postStreamProvider = StreamProvider.autoDispose
+    .family<PostDetail, Tuple3<String?, String?, String?>>((ref, arg) {
   final repository = ref.read(postRepositoryProvider);
 
-  final location = postInfo.item1;
-  final category = postInfo.item2;
-  final postId = postInfo.item3;
+  final location = arg.item1;
+  final category = arg.item2;
+  final postId = arg.item3;
 
   if (location == null || category == null || postId == null) {
     throw Exception('location/category/postId가 설정되지 않았습니다.');
