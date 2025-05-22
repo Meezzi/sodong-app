2f4d023c7b99a60db0723ce415801ac5c212648c
Meezzi <alswl-3016@naver.com>
2025-05-22 11:58:21 +0900

    fix: imageUrl을 nullable로 변경하고 오류 위젯 처리 개선

--- lib/features/post_detail/presentation/widgets/detail_image_view.dart
+++ lib/features/post_detail/presentation/widgets/detail_image_view.dart
@@ -1,25 +1,26 @@
 import 'package:flutter/material.dart';
 
 class DetailImageView extends StatelessWidget {
-
   const DetailImageView({super.key, required this.imageUrl});
-  final String imageUrl;
+  final String? imageUrl;
+
+  Widget get errorWidget => Container(
+        height: 200,
+        color: Colors.grey[300],
+        child: Icon(Icons.broken_image, size: 50),
+      );
 
   @override
   Widget build(BuildContext context) {
     return ClipRRect(
       borderRadius: BorderRadius.circular(12),
-      child: Image.network(
-        imageUrl,
-        height: 200,
-        width: double.infinity,
-        fit: BoxFit.cover,
-        errorBuilder: (_, __, ___) => Container(
-          height: 200,
-          color: Colors.grey[300],
-          child: Icon(Icons.broken_image, size: 50),
-        ),
-      ),
+      child: imageUrl == null
+          ? errorWidget
+          : Image.network(imageUrl!,
+              height: 200,
+              width: double.infinity,
+              fit: BoxFit.cover,
+              errorBuilder: (_, __, ___) => errorWidget),
     );
   }
 }
