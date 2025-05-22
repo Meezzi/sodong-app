f46343167a23ae51476a99edea9bfa242f50e1e2
Meezzi <alswl-3016@naver.com>
2025-05-22 19:34:00 +0900

    fix: ProfileRepositoryProvider에서 Datasource에 대한 의존성을 provider로 주입

--- lib/features/profile/data/providers/profile_provider.dart
+++ lib/features/profile/data/providers/profile_provider.dart
@@ -1,11 +1,10 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:sodong_app/features/profile/data/data_sources/profile_data_source.dart';
 import 'package:sodong_app/features/profile/data/repositories/profile_repository_impl.dart';
+import 'package:sodong_app/features/profile/domain/repositories/profile_repository.dart';
 
 /// Firestore 기반 UserRepository DI 등록
-final profileRepositoryProvider = Provider((ref) {
-  final firestore = FirebaseFirestore.instance;
-  final dataSource = ProfileDataSource(firestore);
+final profileRepositoryProvider = Provider<ProfileRepository>((ref) {
+  final dataSource = ref.watch(profileDataSourceProvider);
   return ProfileRepositoryImpl(dataSource);
 });
