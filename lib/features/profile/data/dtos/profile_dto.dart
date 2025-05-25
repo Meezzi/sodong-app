118e858d6dbbf07538c3c2c1c9dcfa8d1b041114
jinsung99123 <felixson99@gmail.com>
2025-05-25 21:52:55 +0900

    fix: Profile Page UI 조정

--- lib/features/profile/data/dtos/profile_dto.dart
+++ lib/features/profile/data/dtos/profile_dto.dart
@@ -7,28 +7,33 @@ class ProfileDto {
     required this.imageUrl,
     required this.location,
   });
+
   final String nickname;
   final String imageUrl;
   final String location;
 
+  // JSON -> DTO 변환 시 profileImageUrl 키 사용
   factory ProfileDto.fromJson(Map<String, dynamic> json) => ProfileDto(
         nickname: json['nickname'],
-        imageUrl: json['imageUrl'],
+        imageUrl: json['profileImageUrl'], // profileImageUrl로 수정
         location: json['location'],
       );
 
+  // DTO -> JSON 변환 시 profileImageUrl 키 사용
   Map<String, dynamic> toJson() => {
         'nickname': nickname,
-        'imageUrl': imageUrl,
+        'profileImageUrl': imageUrl, // profileImageUrl로 수정
         'location': location,
       };
 
+  // DTO -> Entity 변환
   Profile toEntity() => Profile(
         nickname: nickname,
         imageUrl: imageUrl,
         location: location,
       );
 
+  // Entity -> DTO 변환
   factory ProfileDto.fromEntity(Profile entity) => ProfileDto(
         nickname: entity.nickname,
         imageUrl: entity.imageUrl,
