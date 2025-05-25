d772e2af4f73b54676ea62db5882c9b21c463920
jinsung99123 <felixson99@gmail.com>
2025-05-25 18:04:06 +0900

    fix: 익명 기능 수정-header 위젯 수정

--- lib/features/post_detail/presentation/widgets/detail_header.dart
+++ lib/features/post_detail/presentation/widgets/detail_header.dart
@@ -1,35 +1,37 @@
 import 'package:flutter/material.dart';
 
 class DetailHeader extends StatelessWidget {
-  final bool isAnonymous;
-  final String? nickname;
-  final String? profileImageUrl;
-
   const DetailHeader({
     super.key,
     required this.isAnonymous,
     this.nickname,
     this.profileImageUrl,
   });
+  final bool isAnonymous;
+  final String? nickname;
+  final String? profileImageUrl;
 
   @override
   Widget build(BuildContext context) {
     return Row(
       children: [
-        CircleAvatar(
-          radius: 20,
-          backgroundImage: NetworkImage(
-            isAnonymous
-                ? 'https://example.com/default_profile.png'
-                : (profileImageUrl ??
-                    'https://example.com/default_profile.png'),
-          ),
-        ),
+        isAnonymous
+            ? CircleAvatar(
+                radius: 20,
+                backgroundColor: Colors.pink.shade200,
+                child: const Icon(Icons.person, size: 16, color: Colors.white),
+              )
+            : CircleAvatar(
+                radius: 20,
+                backgroundImage: NetworkImage(
+                  profileImageUrl ?? 'https://example.com/default_profile.png',
+                ),
+              ),
         const SizedBox(width: 12),
         Text(
           isAnonymous ? '익명' : (nickname ?? '알 수 없음'),
           style: const TextStyle(fontWeight: FontWeight.bold, fontSize: 16),
-        )
+        ),
       ],
     );
   }
