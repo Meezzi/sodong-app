d44e93647b7e3686baa981038f7c7b827604da3c
Meezzi <alswl-3016@naver.com>
2025-06-01 20:01:22 +0900

    refactor: PostDetailRepositoryImpl를 post 하위 폴더로 이동

---
+++ lib/features/post/data/repositories/post_detail_repository_impl.dart
@@ -0,0 +1,23 @@
+import 'package:sodong_app/features/post/data/data_source/remote_post_detail_data_source.dart';
+import 'package:sodong_app/features/post/data/mapper/post_mapper.dart';
+import 'package:sodong_app/features/post/domain/entities/post.dart';
+import 'package:sodong_app/features/post/domain/repository/post_detail_repository.dart';
+import 'package:sodong_app/features/post_list/domain/models/category.dart';
+
+class PostDetailRepositoryImpl implements PostDetailRepository {
+  PostDetailRepositoryImpl(this.remoteDataSource, this._postMapper);
+
+  final RemotePostDetailDataSource remoteDataSource;
+  final PostMapper _postMapper;
+
+  @override
+  Stream<Post> getPostDetail(
+    String location,
+    TownLifeCategory category,
+    String postId,
+  ) {
+    return remoteDataSource
+        .getPostDetail(location, category, postId)
+        .map((postDto) => _postMapper.fromDto(postDto));
+  }
+}
