07b8a14e0412ea80d417265b7afe84f32c28ea81
Meezzi <alswl-3016@naver.com>
2025-06-01 13:31:10 +0900

    feat: ImagePickerUsecase Provider 생성

--- lib/features/post/providers/image_providers.dart
+++ lib/features/post/providers/image_providers.dart
@@ -1,6 +1,7 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:sodong_app/features/post/data/data_source/image_picker_data_source.dart';
+import 'package:sodong_app/features/post/domain/use_case/pick_image_usecase.dart';
 
 final imagePickerProvider = Provider<ImagePicker>((ref) {
   return ImagePicker();
@@ -9,4 +10,9 @@ final imagePickerProvider = Provider<ImagePicker>((ref) {
 final _imagePickerDataSourceProvider = Provider<ImagePickerDataSource>((ref) {
   final imagePicker = ref.read(imagePickerProvider);
   return ImagePickerDataSource(imagePicker);
+});
+
+final _imagePickerUsecaseProvider = Provider((ref) {
+  final repository = ref.read(_imagePickerDataSourceProvider);
+  return ImagePickerUsecase(repository);
 });
