46171e96b4d6930c76ac4059f4dae6a88b2e2dd1
Meezzi <alswl-3016@naver.com>
2025-06-01 20:22:06 +0900

    feat: 기존의 GetPostDetailUseCase를 제거하고 FetchPostDetailUseCase 이름 변경

---
+++ lib/features/post/domain/use_case/fetch_post_detail_usecase.dart
@@ -0,0 +1,12 @@
+import 'package:sodong_app/features/post/domain/entities/post.dart';
+import 'package:sodong_app/features/post/domain/repository/post_detail_repository.dart';
+import 'package:sodong_app/features/post_list/domain/models/category.dart';
+
+class FetchPostDetailUseCase {
+  FetchPostDetailUseCase(this.repository);
+  final PostDetailRepository repository;
+
+  Stream<Post> call(String location, TownLifeCategory category, String postId) {
+    return repository.getPostDetail(location, category, postId);
+  }
+}
