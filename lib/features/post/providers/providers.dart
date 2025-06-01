6dd8d61f6afd2b334afa671af8cfdd94e39b048a
Meezzi <alswl-3016@naver.com>
2025-06-01 11:23:04 +0900

    feat: PostRepository provider 추가

--- lib/features/post/providers/providers.dart
+++ lib/features/post/providers/providers.dart
@@ -1,11 +1,22 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:sodong_app/features/create_post/domain/usecase/create_post_usecase.dart';
 import 'package:sodong_app/features/post/data/data_source/post_data_source.dart';
 import 'package:sodong_app/features/post/data/data_source/remote_post_data_source.dart';
+import 'package:sodong_app/features/post/data/repositories/remote_post_repository.dart';
+import 'package:sodong_app/features/post/domain/repository/post_repository.dart';
 
 final _postDatasourceProvider = Provider<PostDataSource>((ref) {
   final firestore = FirebaseFirestore.instance;
   final storage = FirebaseStorage.instance;
   return RemotePostDataSource(firestore, storage);
 });
+
+final _postRepositoryProvider = Provider<PostRepository>(
+  (ref) {
+    final datasource = ref.read(_postDatasourceProvider);
+    return RemotePostRepository(datasource);
+  },
+);
+
