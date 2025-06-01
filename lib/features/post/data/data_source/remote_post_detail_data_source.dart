4840ef061c3b93c8d3250ebe05200cd0491bb82d
Meezzi <alswl-3016@naver.com>
2025-06-01 19:53:15 +0900

    refactor: PostDetailModel을 PostDto로 변경하고 category 타입을 TownLifeCategory로 수정

--- lib/features/post/data/data_source/remote_post_detail_data_source.dart
+++ lib/features/post/data/data_source/remote_post_detail_data_source.dart
@@ -1,32 +1,33 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:sodong_app/features/post/data/data_source/post_detail_data_source.dart';
-import 'package:sodong_app/features/post_detail/data/dtos/post_detail_dto.dart';
+import 'package:sodong_app/features/post/data/dto/post_dto.dart';
+import 'package:sodong_app/features/post_list/domain/models/category.dart';
 
 class RemotePostDetailDataSource implements PostDetailDataSource {
-  
   @override
-  Stream<PostDetailModel> getPostDetail(
-      String location, String category, String postId) {
+  Stream<PostDto> getPostDetail(
+      String location, TownLifeCategory category, String postId) {
     final postDocRef = FirebaseFirestore.instance
         .collection('posts')
         .doc(location.trim())
-        .collection(category)
+        .collection(category.id)
         .doc(postId);
 
     return postDocRef.snapshots().asyncMap((docSnapshot) async {
       if (!docSnapshot.exists) {
-        return PostDetailModel(
+        return PostDto(
           postId: postId,
+          category: category,
           title: '게시물을 찾을 수 없습니다',
           content: '요청하신 게시물이 존재하지 않거나 삭제되었습니다.',
-          imageUrl: [],
-          location: location,
-          category: category,
+          region: location,
           createdAt: DateTime.now(),
+          isAnonymous: true,
           userId: '',
           nickname: '익명',
+          commentCount: 0,
           profileImageUrl: 'https://yourdomain.com/default_anonymous.png',
-          isAnonymous: true,
+          imageUrls: [],
         );
       }
 
@@ -47,9 +48,18 @@ class RemotePostDetailDataSource implements PostDetailDataSource {
             'https://yourdomain.com/default_profile.png';
       }
 
-      return PostDetailModel.fromJson(
-        data,
+      return PostDto(
+        postId: postId,
+        category: category,
+        title: data['title'] ?? '',
+        content: data['content'] ?? '',
+        region: data['region'] ?? '',
+        createdAt: (data['createdAt'] as Timestamp).toDate(),
+        isAnonymous: isAnonymous,
+        userId: userId,
         nickname: nickname,
+        commentCount: data['commentCount'] ?? 0,
+        imageUrls: List<String>.from(data['imageUrls'] ?? []),
         profileImageUrl: profileImageUrl,
       );
     });
