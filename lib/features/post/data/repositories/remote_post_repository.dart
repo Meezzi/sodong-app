6c690f06546f1dbddd8ac6904bd22a9e116526f5
Meezzi <alswl-3016@naver.com>
2025-06-01 19:08:36 +0900

    feat: createPostWithImages에서 PostMapper를 사용하여 DTO 변환 추가

--- lib/features/post/data/repositories/remote_post_repository.dart
+++ lib/features/post/data/repositories/remote_post_repository.dart
@@ -3,14 +3,16 @@ import 'package:firebase_storage/firebase_storage.dart';
 import 'package:sodong_app/core/result/create_post_exception.dart';
 import 'package:sodong_app/core/result/result.dart';
 import 'package:sodong_app/features/post/data/data_source/create_post_data_source.dart';
+import 'package:sodong_app/features/post/data/mapper/post_mapper.dart';
 import 'package:sodong_app/features/post/domain/entities/post.dart';
 import 'package:sodong_app/features/post/domain/repository/post_repository.dart';
 import 'package:sodong_app/features/post_list/domain/models/category.dart';
 
 class RemotePostRepository implements PostRepository {
-  RemotePostRepository(this._postDataSource);
+  RemotePostRepository(this._postDataSource, this._postMapper);
 
   final CreatePostDataSource _postDataSource;
+  final PostMapper _postMapper;
 
   /// Post 저장
   /// DataSource 내부에서 이미지 업로드 처리
@@ -22,8 +24,16 @@ class RemotePostRepository implements PostRepository {
     Post post,
   ) async {
     try {
-      final createdPost = await _postDataSource.createPostWithImages(
-          location, category, imagePaths, post);
+      final postDto = _postMapper.toDto(post);
+
+      final createdPostDto = await _postDataSource.createPostWithImages(
+        location,
+        category,
+        imagePaths,
+        postDto,
+      );
+
+      final createdPost = _postMapper.fromDto(createdPostDto);
 
       return Result.ok(createdPost);
     } on FirebaseException catch (e) {
