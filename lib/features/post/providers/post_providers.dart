d1ec41aaf4c1dace93ef57b2779a00961dd46c7b
Meezzi <alswl-3016@naver.com>
2025-06-01 13:21:37 +0900

    rename: providers.dart에서 post 관련 provider만 모아놓은 post_providers.dart로 파일 이름 수정

--- lib/features/post/providers/post_providers.dart
+++ lib/features/post/providers/post_providers.dart
@@ -1,11 +1,11 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:sodong_app/features/post/domain/use_case/create_post_use_case.dart';
 import 'package:sodong_app/features/post/data/data_source/post_data_source.dart';
 import 'package:sodong_app/features/post/data/data_source/remote_post_data_source.dart';
 import 'package:sodong_app/features/post/data/repositories/remote_post_repository.dart';
 import 'package:sodong_app/features/post/domain/repository/post_repository.dart';
+import 'package:sodong_app/features/post/domain/use_case/create_post_use_case.dart';
 
 final _postDatasourceProvider = Provider<PostDataSource>((ref) {
   final firestore = FirebaseFirestore.instance;
