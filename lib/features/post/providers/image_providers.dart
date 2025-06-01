d26d5aa083679c9c0e7c72cd24bdb88b91efa879
Meezzi <alswl-3016@naver.com>
2025-06-01 13:56:02 +0900

    feat: ImagePickerViewModelProvider 생성

--- lib/features/post/providers/image_providers.dart
+++ lib/features/post/providers/image_providers.dart
@@ -2,6 +2,7 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:sodong_app/features/post/data/data_source/image_picker_data_source.dart';
 import 'package:sodong_app/features/post/domain/use_case/pick_image_usecase.dart';
+import 'package:sodong_app/features/post/presentation/view_models/image_picker_view_model.dart';
 
 final imagePickerProvider = Provider<ImagePicker>((ref) {
   return ImagePicker();
@@ -12,7 +13,12 @@ final _imagePickerDataSourceProvider = Provider<ImagePickerDataSource>((ref) {
   return ImagePickerDataSource(imagePicker);
 });
 
-final _imagePickerUsecaseProvider = Provider((ref) {
+final imagePickerUsecaseProvider = Provider((ref) {
   final repository = ref.read(_imagePickerDataSourceProvider);
   return ImagePickerUsecase(repository);
+});
+
+final imagePickerViewModelProvider =
+    AutoDisposeNotifierProvider<ImagePickerViewModel, ImagePickerState>(() {
+  return ImagePickerViewModel();
 });
