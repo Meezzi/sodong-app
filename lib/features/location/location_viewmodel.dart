0ec54e267c818c93a958f65fdf811eba5d145fa9
dlworua <leegaegyum@gmail.com>
2025-06-03 17:32:07 +0900

    fix: 이미 locations 엔티티폴더 내에 있는 중복된 location클래스 객체 제거

--- lib/features/location/location_viewmodel.dart
+++ lib/features/location/location_viewmodel.dart
@@ -3,17 +3,6 @@ import 'package:geolocator/geolocator.dart';
 import 'package:sodong_app/features/locations/data/data_source/vworld_location_datasource_impl.dart';
 import 'package:sodong_app/features/locations/presentation/provider/providers.dart';
 
-class Location {
-  Location({
-    required this.x,
-    required this.y,
-    required this.region,
-  });
-  final double x;
-  final double y;
-  final String? region;
-}
-
 class LocationViewmodel extends Notifier<Location> {
   late final VWorldLocationDataSourceImpl _repository =
       ref.read(vworldLocationDataSourceProvider);
