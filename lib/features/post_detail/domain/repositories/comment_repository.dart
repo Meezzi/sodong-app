321abb846b6cba529f3cf6a0ffe5087f2fd0d92a
jinsung99123 <felixson99@gmail.com>
2025-06-04 14:26:10 +0900

    fix: 레포지토리 수정

--- lib/features/post_detail/domain/repositories/comment_repository.dart
+++ lib/features/post_detail/domain/repositories/comment_repository.dart
@@ -3,8 +3,15 @@ import 'package:sodong_app/features/post_detail/domain/entities/comment_entity.d
 abstract class CommentRepository {
   Stream<List<Comment>> getCommentsStream(
       String location, String category, String postId);
+
   Future<List<Comment>> getComments(
       String location, String category, String postId);
+
   Future<void> addComment(
-      String location, String category, String postId, String content);
+    String location,
+    String category,
+    String postId,
+    String content,
+    String userId,
+  );
 }
