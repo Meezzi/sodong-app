f45b32750a41e787056af1c9122118e3c103f071
jinsung99123 <felixson99@gmail.com>
2025-06-04 14:26:35 +0900

    fix: 레포지토리 구현체 수정

--- lib/features/post_detail/data/repository/comment_repository_impl.dart
+++ lib/features/post_detail/data/repository/comment_repository_impl.dart
@@ -6,7 +6,6 @@ class CommentRepositoryImpl implements CommentRepository {
   CommentRepositoryImpl(this.service);
   final CommentDataSource service;
 
-  /// 댓글 스트림을 받아 Comment 객체 리스트로 변환
   @override
   Stream<List<Comment>> getCommentsStream(
       String location, String category, String postId) {
@@ -19,14 +18,14 @@ class CommentRepositoryImpl implements CommentRepository {
         .map((list) => list
             .map((data) => Comment(
                   id: data['id'],
-                  postId: postId,
+                  postId: data['postId'],
+                  userId: data['userId'],
                   content: data['content'],
                   createdAt: data['createdAt'],
                 ))
             .toList());
   }
 
-  /// 한 번만 댓글 리스트를 받아오는 비동기 메서드
   @override
   Future<List<Comment>> getComments(
       String location, String category, String postId) async {
@@ -38,25 +37,31 @@ class CommentRepositoryImpl implements CommentRepository {
         )
         .first;
 
-    return result.map((data) {
-      return Comment(
-        id: data['id'],
-        postId: postId,
-        content: data['content'],
-        createdAt: data['createdAt'],
-      );
-    }).toList();
+    return result
+        .map((data) => Comment(
+              id: data['id'],
+              postId: data['postId'],
+              userId: data['userId'],
+              content: data['content'],
+              createdAt: data['createdAt'],
+            ))
+        .toList();
   }
 
-  /// 댓글 추가 요청
   @override
   Future<void> addComment(
-      String location, String category, String postId, String content) {
+    String location,
+    String category,
+    String postId,
+    String content,
+    String userId,
+  ) {
     return service.postComment(
       location: location,
       category: category,
       postId: postId,
       content: content,
+      userId: userId,
     );
   }
 }
