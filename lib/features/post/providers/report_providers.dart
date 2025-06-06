d8f4eb9d7310ee4e61358c4f3bca3c2fcf09fbcf
Meezzi <alswl-3016@naver.com>
2025-06-06 14:03:33 +0900

    feat: report_providers에 ReportPostUseCase Provider추가

--- lib/features/post/providers/report_providers.dart
+++ lib/features/post/providers/report_providers.dart
@@ -1,6 +1,7 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:sodong_app/features/post/data/data_source/remote_report_data_source.dart';
 import 'package:sodong_app/features/post/data/repositories/remote_report_repository.dart';
+import 'package:sodong_app/features/post/domain/use_case/report_post_use_case.dart';
 import 'package:sodong_app/features/post_list/presentation/providers/post_providers.dart';
 
 final _reportDataSource = Provider(
@@ -9,4 +10,8 @@ final _reportDataSource = Provider(
 
 final _reportRepository = Provider(
   (ref) => RemoteReportRepository(ref.read(_reportDataSource)),
+);
+
+final _reportPostUseCase = Provider(
+  (ref) => ReportPostUseCase(ref.read(_reportRepository)),
 );
