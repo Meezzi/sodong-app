9911f74d46cb68c03ae31ab1e632918105238f0b
Meezzi <alswl-3016@naver.com>
2025-06-06 13:55:49 +0900

    feat: repositoryProvider 생성

--- lib/features/post/providers/report_providers.dart
+++ lib/features/post/providers/report_providers.dart
@@ -1,7 +1,12 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:sodong_app/features/post/data/data_source/remote_report_data_source.dart';
+import 'package:sodong_app/features/post/data/repositories/remote_report_repository.dart';
 import 'package:sodong_app/features/post_list/presentation/providers/post_providers.dart';
 
 final _reportDataSource = Provider(
   (ref) => RemoteReportDataSource(ref.read(firestoreProvider)),
+);
+
+final _reportRepository = Provider(
+  (ref) => RemoteReportRepository(ref.read(_reportDataSource)),
 );
