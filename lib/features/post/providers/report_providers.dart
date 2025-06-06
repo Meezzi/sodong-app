b4900fdbfec70535ae6ebb2d83a874efe87ce0d2
Meezzi <alswl-3016@naver.com>
2025-06-06 14:05:26 +0900

    feat: reportViewModelProvider 구현

--- lib/features/post/providers/report_providers.dart
+++ lib/features/post/providers/report_providers.dart
@@ -2,6 +2,7 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:sodong_app/features/post/data/data_source/remote_report_data_source.dart';
 import 'package:sodong_app/features/post/data/repositories/remote_report_repository.dart';
 import 'package:sodong_app/features/post/domain/use_case/report_post_use_case.dart';
+import 'package:sodong_app/features/post/presentation/view_models/post_detail_view_model.dart';
 import 'package:sodong_app/features/post_list/presentation/providers/post_providers.dart';
 
 final _reportDataSource = Provider(
@@ -14,4 +15,8 @@ final _reportRepository = Provider(
 
 final _reportPostUseCase = Provider(
   (ref) => ReportPostUseCase(ref.read(_reportRepository)),
+);
+
+final reportViewModelProvider = Provider(
+  (ref) => ReportViewModel(ref.read(_reportPostUseCase)),
 );
